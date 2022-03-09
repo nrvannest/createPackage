60e18e3197b66a49e4431aa1a1ed9121f33866ce
Nikole Vannest <50924274+nrvannest@users.noreply.github.com>
2022-03-09 10:20:36 -0800

    created help doc for fish data

---
+++ R/fish.R
@@ -0,0 +1,11 @@
+#' Fish catch data
+#'
+#' Data from ESM 262 Computing for Environmental Science course. Including names of fish caught.
+#'
+#' @format A dataframe with 300 observations and 1 variable
+#' itemize{
+#' \item data character string
+#' \item fish_types
+#' }
+#'
+'fish'
--- R/fish_function.R
+++ R/fish_function.R
@@ -1,11 +1,13 @@
-#' Function to Write a function that takes a vector of fish names and always returns three items
+#' fish_calc()
+#'
+#' Function that takes vector of fish names and always returns three items
 #' @param vec input vector of items
 #' @return common Most common fish
 #' @return rare the rarest fish
 #' @return total the total number of fish
 
 # Equation
-fish <- function(vec){
+fish_calc <- function(vec){
   common <- which.max(table(vec))
   rare <- which.min(table(vec))
   total <- sum(table(vec))
